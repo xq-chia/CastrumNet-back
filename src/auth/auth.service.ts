b4dc4b4aed5c5a4f58fc17e8d4eb5536024214cc
Zachary Chia <xqchia614@outlook.com>
2023-09-28 15:10:34 +0800

    feature: create and use jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from "@nestjs/jwt";
 import { UsersService } from './../users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(
+      private usersService: UsersService,
+      private JwtService: JwtService) {}
 
     async signIn(username: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(username);
         if (user?.password !== pass) {
           throw new UnauthorizedException();
         }
-        const { password, ...result } = user;
+        const payload = { sub: user.userId, username: user.username }
         // TODO: Generate a JWT and return it here
         // instead of the user object
-        return result;
+        return {
+          accss_token: await this.JwtService.signAsync(payload)
+        };
       }
 }
