b5659dd5996ddd7848f4010f0f555dda8759c24b
Zachary Chia <xqchia614@outlook.com>
2023-09-28 14:54:28 +0800

    feature: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // TODO: configure proxy
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
