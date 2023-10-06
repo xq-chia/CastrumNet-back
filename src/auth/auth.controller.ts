4fd296567c093bb50d1c9f365f86c47d44e2a089
Zachary Chia <xqchia614@outlook.com>
2023-10-06 15:23:15 +0800

    style: eslint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,21 +4,24 @@ import { LoginDto } from '../dto/login-auth.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    async login(@Body() loginDto: LoginDto) {
-        let res = {
-            msg: 'ok',
-            user: {
-                token: await this.authService.login(loginDto.username, loginDto.password),
-                name: loginDto.username,
-                email: `${loginDto.username}@mail.com`,
-                id: 1000,
-                time: new Date().getTime()
-            }
-        }
-        return res;
-    }
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const res = {
+      msg: 'ok',
+      user: {
+        token: await this.authService.login(
+          loginDto.username,
+          loginDto.password,
+        ),
+        name: loginDto.username,
+        email: `${loginDto.username}@mail.com`,
+        id: 1000,
+        time: new Date().getTime(),
+      },
+    };
+    return res;
+  }
 }
