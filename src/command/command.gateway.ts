a69414a4fda3c2369e4f4249398af204503dfc2c
Zachary Chia <xqchia614@outlook.com>
2023-11-03 21:23:24 +0800

    feat: terminal command execution

--- src/command/command.gateway.ts
+++ src/command/command.gateway.ts
@@ -1,23 +1,24 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { MessageBody, OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import * as os from 'os';
-import * as pty from 'node-pty'
+import * as pty from 'node-pty';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class CommandGateway {
+export class CommandGateway implements OnGatewayInit {
   readonly shell: string = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
-  readonly ptyProcess = pty.spawn(this.shell, [], {
+  ptyProcess = pty.spawn(this.shell, [], {
     name: 'xterm-color',
     env: process.env
   })
 
+  afterInit(server: Server) {
+    this.ptyProcess.onData(output => {
+      server.send(output)
+    })
+  }
+
   @SubscribeMessage('message')
   handleMessage(@MessageBody() message: string) {
-    this.ptyProcess.write(message + '\r');
-
-    // buggy
-    this.ptyProcess.onData(output => {
-      console.log('comamnd: ', message)
-      console.log('return: ', output)
-    })
+    this.ptyProcess.write(message);
   }
 }
