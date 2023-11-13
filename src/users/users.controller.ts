b4a07f2b1fa97a71effe2c8f7e3153800878e0c3
Zachary Chia <xqchia614@outlook.com>
2023-11-13 20:55:21 +0800

    feat: view user details

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,27 +2,56 @@ import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateDto } from 'src/dto/create-users.dto';
 import { User } from 'src/entity/user.entity';
+import { Tenant } from 'src/entity/tenant.entity';
+import { TenantsService } from 'src/tenants/tenants.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private tenantsService: TenantsService,
+  ) {}
 
-    @Get()
-    fetchAll(): Promise<User[]> {
-        return this.usersService.findAll();
-    }
+  @Get()
+  fetchAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
 
-    @Post()
-    async create(@Body() createDto: CreateDto): Promise<boolean> {
-        let user: User;
+  @Get(':userId')
+  async fetch(@Param('userId') userId: number) {
+    let user: User;
+    let tenant: Tenant;
+    let ret: any;
 
-        user = new User(createDto.userId, createDto.username, createDto.password, createDto.firstName, createDto.lastName, createDto.tenantId)
-        
-        return await this.usersService.save(user);
-    }
+    user = await this.usersService.findOneById(userId);
+    tenant = await this.tenantsService.findOneById(user.tenantId);
 
-    @Patch('freeze/:userId')
-    async freeze(@Param('userId') userId: any) {
-        return await this.usersService.updateStatus(userId);
-    }
+    ret = {
+      user: user,
+      tenant: tenant
+    };
+
+    return ret;
+  }
+
+  @Post()
+  async create(@Body() createDto: CreateDto): Promise<boolean> {
+    let user: User;
+
+    user = new User(
+      createDto.userId,
+      createDto.username,
+      createDto.password,
+      createDto.firstName,
+      createDto.lastName,
+      createDto.tenantId,
+    );
+
+    return await this.usersService.save(user);
+  }
+
+  @Patch('freeze/:userId')
+  async freeze(@Param('userId') userId: any) {
+    return await this.usersService.updateStatus(userId);
+  }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { DbModule } from 'src/db/db.module';
 import { UsersController } from './users.controller';
+import { TenantsService } from 'src/tenants/tenants.service';
 
 @Module({
-  providers: [UsersService],
+  providers: [UsersService, TenantsService],
   exports: [UsersService],
   imports: [DbModule],
   controllers: [UsersController]
