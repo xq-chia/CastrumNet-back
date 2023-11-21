aeb5b38955a2ff86df45929d1b13ab32993ea650
Zachary Chia <xqchia614@outlook.com>
2023-11-21 15:25:34 +0800

    feat: hostAssignment listing

--- src/host_assignment/host_assignment.controller.ts
+++ src/host_assignment/host_assignment.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { User } from 'src/entity/user.entity';
+import { UsersService } from 'src/users/users.service';
 
-@Controller('host-assignment')
-export class HostAssignmentController {}
+@Controller('hostAssignment')
+export class HostAssignmentController {
+    constructor(private userService: UsersService) {}
+    @Get()
+    async fetch() {
+        let users: User[];
+        let ret: any[] = [];
+
+        users = await this.userService.findAll()
+        for (const user of users) {
+            ret.push({ userId: user.userId, username: user.username });
+        }
+
+        return ret;
+    }
+}
--- src/host_assignment/host_assignment.module.ts
+++ src/host_assignment/host_assignment.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { HostAssignmentController } from './host_assignment.controller';
 import { HostAssignmentService } from './host_assignment.service';
 import { DbModule } from 'src/db/db.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [DbModule],
   controllers: [HostAssignmentController],
-  providers: [HostAssignmentService],
+  providers: [HostAssignmentService, UsersService],
   exports: [HostAssignmentService]
 })
 export class HostAssignmentModule {}
