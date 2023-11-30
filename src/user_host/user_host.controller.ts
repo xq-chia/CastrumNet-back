abe174ab00deca11df7ca77e712fa4d67df95d9c
Zachary Chia <xqchia614@outlook.com>
2023-11-30 16:52:07 +0800

    refactor: userHost standard api response

--- src/user_host/user_host.controller.ts
+++ src/user_host/user_host.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
 import { UserHostService } from './user_host.service';
+import { UserHost } from 'src/entity/user_host.entity';
+import { Host } from 'src/entity/host.entity';
+import { HostService } from 'src/host/host.service';
+import { TransformInterceptor } from 'src/interceptor/transform/transform.interceptor';
 
+@UseInterceptors(TransformInterceptor)
 @Controller('userHost')
 export class UserHostController {
-    constructor(private userHostSrv: UserHostService) {}
+    constructor(
+        private userHostSrv: UserHostService,
+        private hostSrv: HostService
+    ) {}
 
     @Get(':userId')
     async fetch(@Param('userId') userId: number) {
-        return await this.userHostSrv.findAllByUserId(userId);
+        let userHosts: UserHost[];
+        let ret: any[] = [];
+
+        userHosts = await this.userHostSrv.findAllByUserId(userId);
+
+        for (const userHost of userHosts) {
+            let host: Host;
+
+            host = await this.hostSrv.findOneByHostId(userHost.hostId);
+
+            ret.push({
+                userId: userHost.userId,
+                userHostId: userHost.userHostId,
+                host: {
+                    hostId: host.hostId,
+                    host: host.host,
+                    ipAddress: host.ipAddress
+                }
+            })
+        }
+
+        return ret;
     }
 }
--- src/user_host/user_host.module.ts
+++ src/user_host/user_host.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserHostService } from './user_host.service';
 import { DbModule } from 'src/db/db.module';
 import { UserHostController } from './user_host.controller';
+import { HostService } from 'src/host/host.service';
 
 @Module({
   imports: [DbModule],
-  providers: [UserHostService],
+  providers: [UserHostService, HostService],
   exports: [UserHostService],
   controllers: [UserHostController]
 })
