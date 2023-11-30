6db2d2183d325da9eab5d9694a34263cab9c1802
Zachary Chia <xqchia614@outlook.com>
2023-11-30 21:07:14 +0800

    refactor: tenant standard api response

--- src/tenants/tenants.controller.ts
+++ src/tenants/tenants.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { TenantsService } from './tenants.service';
+import { TransformInterceptor } from 'src/interceptor/transform/transform.interceptor';
 
+@UseInterceptors(TransformInterceptor)
 @Controller('tenants')
 export class TenantsController {
     constructor(private tenantService: TenantsService) {}
