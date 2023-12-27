4282d039065179dc2b88fbe30efaafb8d3976938
Zachary Chia <xqchia614@outlook.com>
2023-12-27 20:18:09 +0800

    feat: refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { AuthService } from './auth.service';
-import { Body, Controller, HttpCode, HttpStatus, Post, UseFilters, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Headers, HttpCode, HttpStatus, Post, UseFilters, UseInterceptors } from '@nestjs/common';
 import { LoginDto } from '../dto/login-auth.dto';
 import { UsersService } from 'src/users/users.service';
 import { User } from 'src/entity/user.entity';
 import { TransformInterceptor } from 'src/interceptor/transform/transform.interceptor';
 import { AccountingInterceptor } from 'src/interceptor/accounting/accounting.interceptor';
 import { GenericExceptionFilter } from 'src/filter/generic-exception/generic-exception.filter';
+import { JwtService } from '@nestjs/jwt';
 
 @UseInterceptors(AccountingInterceptor, TransformInterceptor)
 @UseFilters(GenericExceptionFilter)
@@ -14,6 +15,7 @@ export class AuthController {
   constructor(
     private authService: AuthService,
     private usersService: UsersService,
+    private jwtService: JwtService
   ) {}
 
   @HttpCode(HttpStatus.OK)
@@ -43,5 +45,28 @@ export class AuthController {
       };
 
       return res;
+  }
+  
+  @Post('refresh')
+  async refresh(@Headers('token') token: string) {
+    let clearToken: any;
+    let res: any;
+    let user: User;
+    let time: number;
+
+    clearToken = this.jwtService.decode(token);
+    user = await this.usersService.findOneById(clearToken.sub)
+    time = new Date().getTime();
+
+    res = {
+          token: token,
+          name: user.firstName + ' ' + user.lastName,
+          email: user.username,
+          id: user.userId,
+          time: time,
+          expired: time + 1000 * 60 * 5,
+    }
+
+    return res;
   }
 }
