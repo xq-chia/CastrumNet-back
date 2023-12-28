9992c5750f28ff01c9460a4dec756576dde56ade
Zachary Chia <xqchia614@outlook.com>
2023-12-28 20:39:47 +0800

    feat: inheritanceId pk

--- src/entity/role_inheritance.entity.ts
+++ src/entity/role_inheritance.entity.ts
@@ -1,8 +1,10 @@
 export class RoleInheritance {
   roleId: number;
   parentId: number;
+  inheritanceId: number;
 
-  constructor(roleId: number, parentId: number) {
+  constructor(roleId: number, parentId: number, inheritanceId?: number) {
+    this.inheritanceId = inheritanceId;
     this.roleId = roleId;
     this.parentId = parentId ? parentId : -1;
   }
